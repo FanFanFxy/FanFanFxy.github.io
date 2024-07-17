699fc7c2d04de9c75e4f97a97f99af831d74d600
FanFanJJ <1437875082@qq.com>
2024-07-17 21:15:53 +0800

    fix

--- js/memories.js
+++ js/memories.js
@@ -5,30 +5,30 @@ $(function(){
         'navigationColor':['#fff'],
     });
 
-    var li = $('.timeline');
-    var leng = li.length;
-    var i=0;
-
-    function feiru() {
-        if ((leng - 1) > i) {
-            setTimeout(function () {
-                var a=new animateFct(i);
-                console.log(i);
-                feiru();
-                i++;
-            }, 4000);
-        } else {
-            $('.begin').text("");
-            $('.begin').fadeIn();
-            return;
-        }
-    };
-
-    var beg=$('.begin');
-    beg.click(function(){
-        $(this).hide();
-        $('audio')[0].play();
-        feiru();
-    });
+    // var li = $('.timeline');
+    // var leng = li.length;
+    // var i=0;
+    //
+    // function feiru() {
+    //     if ((leng - 1) > i) {
+    //         setTimeout(function () {
+    //             var a=new animateFct(i);
+    //             console.log(i);
+    //             feiru();
+    //             i++;
+    //         }, 4000);
+    //     } else {
+    //         $('.begin').text("");
+    //         $('.begin').fadeIn();
+    //         return;
+    //     }
+    // };
+    //
+    // var beg=$('.begin');
+    // beg.click(function(){
+    //     $(this).hide();
+    //     $('audio')[0].play();
+    //     feiru();
+    // });
 });
-loadAudioFile('../music/2.mp3');
+// loadAudioFile('../music/2.mp3');
